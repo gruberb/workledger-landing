306e95c1505e314d12621caed9cb5c8250e8293a
Bastian Gruber <foreach@me.com>
2026-02-19 11:06:04 -0400

    fix: Inject async part for Plausible in build script

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ mkdir -p out
 cp *.html *.svg out/ 2>/dev/null || true
 cp -r images out/ 2>/dev/null || true
 if [ -n "${PLAUSIBLE_LANDING_SRC:-}" ]; then
-  sed -i'' -e "s#<!-- Analytics injected at deploy time via deploy.sh -->#<script async src=\"$PLAUSIBLE_LANDING_SRC\"></script>#" out/index.html
+  SNIPPET='<script async src="'"$PLAUSIBLE_LANDING_SRC"'"></script><script>window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};plausible.init()</script>'
+  sed -i'' -e "s#<!-- Analytics injected at deploy time via deploy.sh -->#${SNIPPET}#" out/index.html
 fi
